94397905ea64efa527e22900212f8098e09bf586
flindgren <fredrik.lindgren@odigeo.com>
2018-10-05 10:22:07 +0200

    Initial commit from spring initializr

---
+++ src/main/java/flindgren/katas/reactivebowling/ReactiveBowlingApplication.java
@@ -0,0 +1,12 @@
+package flindgren.katas.reactivebowling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveBowlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveBowlingApplication.class, args);
+    }
+}
---
+++ src/test/java/flindgren/katas/reactivebowling/ReactiveBowlingApplicationTests.java
@@ -0,0 +1,16 @@
+package flindgren.katas.reactivebowling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveBowlingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
